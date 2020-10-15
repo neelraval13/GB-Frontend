d0144d86a1495232fa5266bcb2f4f4b394cbf352
Santhosh-Chidambaram <santhoshsmart111@gmail.com>
2020-10-15 23:39:37 +0530

    added route to home & login

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import HomePage from "./components/Home";
 import Login from "./components/Login/Login";
 import "./styles/global/global.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <Login />
-      {/* <HomePage /> */}
-    </>
+   <Router>
+     <Switch>
+       <Route exact path='/'>
+            <Login />
+       </Route>
+       <Route exact path='/home'>
+            <HomePage />
+       </Route>
+     </Switch>
+   </Router>
   );
 };
 
